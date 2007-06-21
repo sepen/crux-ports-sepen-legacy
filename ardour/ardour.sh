c7f641dbe6e1ef30e220685377bc099336828c11
Jose V Beneyto <sepen@crux.nu>
2007-06-21 10:27:32 +0000

    ports/ardour: Updated SVN version -> 2.0.2 (released version). Thanks to Johannes.
    git-svn-id: file:///tmp/crux.sepen.svnrepo/trunk/ports@150 e5078a59-c42a-0410-a761-6d5b026a78ea

--- ardour/ardour.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export LD_LIBRARY_PATH=/usr/lib/ardour2:$LD_LIBRARY_PATH 
-# DYLD_LIBRARY_PATH is for Darwin
-export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
-
-exec /usr/lib/ardour2/ardour.bin $*
-
-# End of File
