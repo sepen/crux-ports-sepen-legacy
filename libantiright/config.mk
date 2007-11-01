3ccb4c4ab89143c427cb88d40ce295b78ac10e81
Jose V Beneyto <sepen@crux.nu>
2007-11-01 00:28:18 +0000

    libantiright: Removed vte as dep.
    git-svn-id: file:///tmp/crux.sepen.svnrepo/trunk/ports@265 e5078a59-c42a-0410-a761-6d5b026a78ea

--- libantiright/config.mk
+++ libantiright/config.mk
@@ -3,9 +3,9 @@ VERSION=3.6
 PREFIX=/usr
 INCLUDE= -I/usr/include/gtk-2.0 -I/usr/lib/gtk-2.0/include -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/freetype2 -I/usr/include/libpng12 -I/usr/include/X11
 LIBDIR= -L/usr/lib/X11
-DEFS= -DPACKAGE_STRING='"AntiRight_3.6"' -DPREFIX='"/usr"' -DUSE_GTK -DHAVE_VTE -D_GNU_SOURCE -DARDATADIR='"/usr/share/antiright"'
+DEFS= -DPACKAGE_STRING='"AntiRight_3.6"' -DPREFIX='"/usr"' -DUSE_GTK -D_GNU_SOURCE -DARDATADIR='"/usr/share/antiright"'
 MAKE=/usr/bin/make
 INSTALL=/usr/bin/install
 CFLAGS= -Wall
 CC=cc
-LDFLAGS=-lvte -lXft -lgtk-x11-2.0 -lXrender -lfontconfig -lfreetype -lz -lX11 -latk-1.0 -lcairo -lpango-1.0 -lgobject-2.0 -lgmodule-2.0 -ldl -lglib-2.0 
+LDFLAGS=-lXft -lgtk-x11-2.0 -lXrender -lfontconfig -lfreetype -lz -lX11 -latk-1.0 -lcairo -lpango-1.0 -lgobject-2.0 -lgmodule-2.0 -ldl -lglib-2.0 
