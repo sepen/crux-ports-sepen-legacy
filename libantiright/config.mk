f70ea392eeb57f35a9737359e67c6a1edbf53d9f
Jose V Beneyto <sepen@crux.nu>
2007-11-01 01:31:26 +0000

    libantiright: Fixed deps and cleaned up.
    git-svn-id: file:///tmp/crux.sepen.svnrepo/trunk/ports@266 e5078a59-c42a-0410-a761-6d5b026a78ea

--- libantiright/config.mk
+++ libantiright/config.mk
@@ -8,4 +8,4 @@ MAKE=/usr/bin/make
 INSTALL=/usr/bin/install
 CFLAGS= -Wall
 CC=cc
-LDFLAGS=-lXft -lgtk-x11-2.0 -lXrender -lfontconfig -lfreetype -lz -lX11 -latk-1.0 -lcairo -lpango-1.0 -lgobject-2.0 -lgmodule-2.0 -ldl -lglib-2.0 
+LDFLAGS= -lXft -lgtk-x11-2.0 -lXrender -lfontconfig -lfreetype -lz -lX11 -latk-1.0 -lcairo -lpango-1.0 -lgobject-2.0 -lgmodule-2.0 -ldl -lglib-2.0 
