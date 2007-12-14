727564821514dcfb9d5a7df94f99b2f00f730de6
Jose V Beneyto <sepen@crux.nu>
2007-12-14 18:43:22 +0000

    gtkshell: Updated 3.6-1 -> 3.6-2
    git-svn-id: file:///tmp/crux.sepen.svnrepo/trunk/ports@371 e5078a59-c42a-0410-a761-6d5b026a78ea

--- gtkshell/config.mk
+++ gtkshell/config.mk
@@ -1,11 +1,16 @@
-VTE=1
-VERSION=3.6
-PREFIX=/usr
-INCLUDE= -I. -I/usr/include/antiright -I/usr/include/gtk-2.0 -I/usr/lib/gtk-2.0/include -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/freetype2 -I/usr/include/libpng12 -I/usr/include/X11
-LIBDIR= -L/usr/lib/X11 -L/usr/lib/antiright
-DEFS= -DPACKAGE_STRING='"AntiRight_3.6"' -DPREFIX='"/usr"' -DUSE_GTK -D_GNU_SOURCE -DARDATADIR='"/usr/share/antiright"'
 MAKE=/usr/bin/make
 INSTALL=/usr/bin/install
-CFLAGS= -Wall
+
+NAME=gtkshell
+VERSION=3.6
+PREFIX=/usr
+ARDATADIR=/usr/share/antiright
+
 CC=cc
-LDFLAGS=-lantiright -lXft -lgtk-x11-2.0 -lXrender -lfontconfig -lfreetype -lz -lX11 -latk-1.0 -lcairo -lpango-1.0 -lgobject-2.0 -lgmodule-2.0 -ldl -lglib-2.0 $(shell pkg-config --libs vte)
+
+DEFS=-DPACKAGE_STRING='"$(NAME) $(VERSION)"' -DPREFIX='"$(PREFIX)"' -DUSE_GTK -D_GNU_SOURCE -DARDATADIR='"$(ARDATADIR)"'
+
+LDFLAGS=$(shell pkg-config --libs gtk+-2.0)
+LIBDIR=-L/usr/lib -L/usr/lib/X11
+INCLUDE=$(shell pkg-config --cflags gtk+-2.0)
+CFLAGS=-Wall $(INCLUDE)
