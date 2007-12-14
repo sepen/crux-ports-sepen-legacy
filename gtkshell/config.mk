2a4089e963771ae4102c4b9e1bc5939d124640d3
Jose V Beneyto <sepen@crux.nu>
2007-12-14 19:15:42 +0000

    ports/gtkshell: Full review of the port. Added also xshell binary.
    git-svn-id: file:///tmp/crux.sepen.svnrepo/trunk/ports@372 e5078a59-c42a-0410-a761-6d5b026a78ea

--- gtkshell/config.mk
+++ gtkshell/config.mk
@@ -6,11 +6,13 @@ VERSION=3.6
 PREFIX=/usr
 ARDATADIR=/usr/share/antiright
 
+VTE=1
+
 CC=cc
 
 DEFS=-DPACKAGE_STRING='"$(NAME) $(VERSION)"' -DPREFIX='"$(PREFIX)"' -DUSE_GTK -D_GNU_SOURCE -DARDATADIR='"$(ARDATADIR)"'
 
-LDFLAGS=$(shell pkg-config --libs gtk+-2.0)
+LDFLAGS=$(shell pkg-config --libs gtk+-2.0; pkg-config --libs vte)
 LIBDIR=-L/usr/lib -L/usr/lib/X11
-INCLUDE=$(shell pkg-config --cflags gtk+-2.0)
+INCLUDE=$(shell pkg-config --cflags gtk+-2.0; pkg-config --cflags vte)
 CFLAGS=-Wall $(INCLUDE)
